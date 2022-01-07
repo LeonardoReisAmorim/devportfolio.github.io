06d622cc6eb8c9fffa7a3cedfd3b2c5b7414cb86
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2022-01-07 01:57:22 -0300

    portfoliov1

---
+++ source/js/index.js
@@ -0,0 +1,11 @@
+document.getElementById("navSide").addEventListener('mouseover', ()=>{
+    document.getElementById("divlinks").style.visibility = "visible";
+    document.getElementById("divlinks").style.opacity = 1;
+
+    document.getElementById("links").style.visibility = "visible"
+})
+
+document.getElementById("navSide").addEventListener('mouseout', ()=>{
+    document.getElementById("divlinks").style.visibility = "hidden";
+    document.getElementById("divlinks").style.opacity = 0;
+})
