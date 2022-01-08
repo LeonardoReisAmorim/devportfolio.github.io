b53892c16680d562de2ccece3bc80b235d5b6710
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2022-01-07 21:06:21 -0300

    portfoliov3

--- source/js/index.js
+++ source/js/index.js
@@ -1,12 +1,77 @@
 document.getElementById("navSide").addEventListener('mouseover', ()=>{
     document.getElementById("divlinks").style.visibility = "visible";
     document.getElementById("divlinks").style.opacity = 1;
-
-    document.getElementById("links").style.visibility = "visible"
-
+    
 })
+document.getElementsByClassName("ball")[0].addEventListener('mouseover', ()=>{
+    document.getElementsByClassName("liA")[0].style.visibility = "visible";
+    document.getElementsByClassName("liA")[0].animate([
+        { transform: 'translateY(200px)', opacity: 0 },
+        { transform: 'translateY(-5px)', opacity: 1 },
+        { transform: 'translateY(10px)'},
+        { transform: 'translateY(0)' }
+    ],{
+        duration: 1500,
+    })
+});
+document.getElementsByClassName("ball")[1].addEventListener('mouseover', ()=>{
+    document.getElementsByClassName("liA")[1].style.visibility = "visible";
+    document.getElementsByClassName("liA")[1].animate([
+        { transform: 'translateY(200px)', opacity: 0 },
+        { transform: 'translateY(-5px)', opacity: 1 },
+        { transform: 'translateY(10px)'},
+        { transform: 'translateY(0)' }
+    ],{
+        duration: 1500,
+    })
+});
+document.getElementsByClassName("ball")[2].addEventListener('mouseover', ()=>{
+    document.getElementsByClassName("liA")[2].style.visibility = "visible";
+    document.getElementsByClassName("liA")[2].animate([
+        { transform: 'translateY(200px)', opacity: 0 },
+        { transform: 'translateY(-5px)', opacity: 1 },
+        { transform: 'translateY(10px)'},
+        { transform: 'translateY(0)' }
+    ],{
+        duration: 1500,
+    })
+});
+document.getElementsByClassName("ball")[3].addEventListener('mouseover', ()=>{
+    document.getElementsByClassName("liA")[3].style.visibility = "visible";
+    document.getElementsByClassName("liA")[3].animate([
+        { transform: 'translateY(200px)', opacity: 0 },
+        { transform: 'translateY(-5px)', opacity: 1 },
+        { transform: 'translateY(10px)'},
+        { transform: 'translateY(0)' }
+    ],{
+        duration: 1500,
+    })
+});
+document.getElementsByClassName("ball")[4].addEventListener('mouseover', ()=>{
+    document.getElementsByClassName("liA")[4].style.visibility = "visible";
+    document.getElementsByClassName("liA")[4].animate([
+        { transform: 'translateY(200px)', opacity: 0 },
+        { transform: 'translateY(-5px)', opacity: 1 },
+        { transform: 'translateY(10px)'},
+        { transform: 'translateY(0)' }
+    ],{
+        duration: 1500,
+    })
+});
+document.getElementsByClassName("ball")[5].addEventListener('mouseover', ()=>{
+    document.getElementsByClassName("liA")[5].style.visibility = "visible";
+    document.getElementsByClassName("liA")[5].animate([
+        { transform: 'translateY(200px)', opacity: 0 },
+        { transform: 'translateY(-5px)', opacity: 1 },
+        { transform: 'translateY(10px)'},
+        { transform: 'translateY(0)' }
+    ],{
+        duration: 1500,
+    })
+});
 
 document.getElementById("navSide").addEventListener('mouseout', ()=>{
     document.getElementById("divlinks").style.visibility = "hidden";
     document.getElementById("divlinks").style.opacity = 0;
 })
+
